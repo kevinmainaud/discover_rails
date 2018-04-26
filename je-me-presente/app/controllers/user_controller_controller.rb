b7408f662d380bfda47e6d79478583505c247dd1
kevinmainaud <kevin.mainaud@gmail.com>
2018-04-27 01:11:47 +0200

    V04

--- je-me-presente/app/controllers/user_controller_controller.rb
+++ je-me-presente/app/controllers/user_controller_controller.rb
@@ -1,4 +1,17 @@
 class UserControllerController < ApplicationController
-def new
-User.create
+    def new
+        @user = User.new
+    end
+        def create
+            @user = User.new(name: params["user_name"], bio: params["bio"])
+            if @user.save
+                redirect_to user_path(@user.name)
+            else
+                render 'new'
+            end
+        end
+    
+        def show 
+            @user = User.find_by_name(params[:name])
+        end
 end
--- je-me-presente/config/routes.rb
+++ je-me-presente/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get '/',to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact', as: "contact_us"
   get '/about', to: 'static_pages#about', as: "about_us"
-  get '/about/toi', to: 'static_pages#about#toi', as:"about_me"
-  get '/about/ton_groupe', to: 'static_pages#about#ton_groupe', as: "about_group"
-  get '/new', to: 'users_controller#new', as:"new_user"
+  get '/about/toi', to: 'static_pages#about#toi', as:"toi"
+  get '/about/ton_groupe', to: 'static_pages#about#ton_groupe', as: "ton_groupe"
+  get '/new', to: 'user_controller#new', as:"new_user"
+  get '/users/:name', to: 'user_controller#show' , as: 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
